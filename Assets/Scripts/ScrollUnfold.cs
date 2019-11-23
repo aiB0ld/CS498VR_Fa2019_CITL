38bd9b39a5dc9e37a262623ee8d71c058018fadc
LEON-HYZ <56896322+LEON-HYZ@users.noreply.github.com>
2019-11-22 19:09:26 -0600

    update island model

---
+++ Assets/Scripts/ScrollUnfold.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUnfold : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void Unfold()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
