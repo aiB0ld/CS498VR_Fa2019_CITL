f5b6db8a4513b60b7b67c0e7b18f85618c222382
aiB0ld <48540959+aiB0ld@users.noreply.github.com>
2019-11-30 09:38:34 -0600

    ui3.0 update

---
+++ Assets/Resources/Scripts/AirplaneController.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AirplaneController : MonoBehaviour {
+
+    private bool isAlive;
+    private bool isPaused;
+    private float moveSpeed = 30f;
+    // private float rotateSpeed = 3f;
+    private GameObject spitfire;
+    public Text scoreBoard;
+    public GameObject gameOverPanel;
+    public GameObject pausePanel;
+    public GameObject tutorialPanel;
+    public Text tutorialText;
+    private int tutorial_2_counter = 0;
+    private int tutorial_3_counter = 0;
+
+    private AudioClip MusicClip_GameOver;
+    private AudioClip MusicClip_Engine;
+    private AudioClip MusicClip_Ambient;
+    private AudioSource MusicSource;
+
+    // Use this for initialization
+    void Start () {
+        isAlive = true;
+        isPaused = false;
+        gameOverPanel.SetActive(false);
+        pausePanel.SetActive(false);
+        tutorialPanel.SetActive(false);
+        spitfire = GameObject.Find("Super_Spitfire");
+        MusicSource = GetComponent<AudioSource>();
+
+        MusicClip_GameOver = Resources.Load<AudioClip>("Sounds/GameOver");
+        MusicClip_Engine = Resources.Load<AudioClip>("Sounds/Engine");
+        MusicClip_Ambient = Resources.Load<AudioClip>("Sounds/Ambient");
+        
+        if (PlayMode.mode == "Tutorial")
+        {
+            tutorialPanel.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (tutorialPanel.activeInHierarchy)
+        {
+            if (PlayMode.mode == "Tutorial")
+            {
+                tutorialText.text = "Welcome to the tutorial mode, the minions will guide through the control shcemes step by step. \n\nFollow the instructions and complete the tutorial \n\nPress B at anytime to quit the tutorial \nTake your time and enjoy a great flight! \n\nNow, Press A to move on";
+                if (OVRInput.Get(OVRInput.RawButton.A))
+                {
+                    PlayMode.mode = "Tutorial_2";
+                }
+                else if (OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    resetSpitfire();
+                }
+            }
+            else if (PlayMode.mode == "Tutorial_2")
+            {
+                tutorialText.text = "Awesome! Now rotate your right touch controller and feel the plane. \n\nWarning: Please don't over-rotate or you might feel uncomfortable with it";
+                if (tutorial_2_counter <= 500)
+                {
+                    if (transform.rotation != OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch))
+                    {
+                        tutorial_2_counter += 1;
+                    }
+                    transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+                }
+                else
+                {
+                    tutorial_2_counter = 0;
+                    PlayMode.mode = "Tutorial_3";
+                }
+
+                if (OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    resetSpitfire();
+                }
+            }
+            else if (PlayMode.mode == "Tutorial_3")
+            {
+                tutorialText.text = "Great! Now push/pull your right thumbstick and feel the acceleration/deceleration";
+                if (tutorial_3_counter <= 500)
+                {
+                    float acceleration = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x;
+                    if (acceleration != 0)
+                    {
+                        tutorial_3_counter += 1;
+                    }
+                    spitfire.GetComponent<Rigidbody>().velocity = spitfire.transform.forward * moveSpeed;
+                    if (moveSpeed + acceleration > 30f || moveSpeed + acceleration < 200f)
+                    {
+                        moveSpeed += acceleration;
+                    }
+                    else
+                    {
+                        moveSpeed = 30f;
+                    }
+                    transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+                }
+                else
+                {
+                    tutorial_3_counter = 0;
+                    PlayMode.mode = "Tutorial_4";
+                }
+
+                if (OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    resetSpitfire();
+                }
+                
+            }
+            else if (PlayMode.mode == "Tutorial_4")
+            {
+                tutorialText.text = "Cool! Now you are ready for the game. \n\nPress X to shoot the spheres and reach a high score! \nYour score is shown on the left wing \n\nReminder: Press Y at anytime to pause the game \n\nPress A to end the tutorial and start the game";
+                spitfire.transform.position = new Vector3(22f, 456, -904);
+                spitfire.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                spitfire.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                spitfire.transform.rotation = Quaternion.identity;
+                if (OVRInput.Get(OVRInput.RawButton.A) || OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    resetSpitfire();
+                }
+            }
+        }
+        else
+        {
+            if (isAlive && !isPaused)
+            {
+                // Plane movement control
+                float angle = 0.0f;
+                Vector3 axis = Vector3.zero;
+                OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).ToAngleAxis(out angle, out axis);
+                Vector3 angles = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles;
+
+                spitfire.GetComponent<Rigidbody>().velocity = spitfire.transform.forward * moveSpeed;
+                // transform.RotateAround(transform.position, axis, 0.004f * angle);
+                // transform.rotation = Quaternion.Lerp(Quaternion.identity, OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch), 0.25f);
+                transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+                // transform.Rotate(angles.x * 0.0005f, angles.y * 0.0005f, angles.z * 0.0005f);
+
+                // Acceleration of Airplane
+                float acceleration = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).x;
+                if (moveSpeed + acceleration > 30f || moveSpeed + acceleration < 200f)
+                {
+                    moveSpeed += acceleration;
+                }
+                else
+                {
+                    moveSpeed = 30f;
+                }
+
+                // Pause while in game
+                if (OVRInput.Get(OVRInput.RawButton.Y))
+                {
+                    isPaused = true;
+                    pausePanel.SetActive(true);
+                    spitfire.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    spitfire.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                }
+            }
+
+            if (pausePanel.activeInHierarchy)
+            {
+                if (OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    MusicSource.clip = MusicClip_Engine;
+                    MusicSource.Play(0);
+                    isPaused = false;
+                    pausePanel.SetActive(false);
+                }
+                else if (OVRInput.Get(OVRInput.RawButton.A))
+                {
+                    MusicSource.clip = MusicClip_Ambient;
+                    MusicSource.Play(0);
+                    SceneManager.LoadScene("StartMenu");
+                }
+            }
+            else if (gameOverPanel.activeInHierarchy)
+            {
+                if (OVRInput.Get(OVRInput.RawButton.B))
+                {
+                    MusicSource.clip = MusicClip_Engine;
+                    MusicSource.Play(0);
+                    scoreBoard.text = "Score: 0";
+                    gameOverPanel.SetActive(false);
+                    isAlive = true;
+                    moveSpeed = 30f;
+                }
+                else if (OVRInput.Get(OVRInput.RawButton.A))
+                {
+                    MusicSource.clip = MusicClip_Engine;
+                    MusicSource.Play(0);
+                    SceneManager.LoadScene("StartMenu");
+                }
+                else if (OVRInput.Get(OVRInput.RawButton.Y))
+                {
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#else
+                        Application.Quit();
+#endif
+                }
+            }
+        }
+    }
+
+    private void resetSpitfire()
+    {
+        isAlive = true;
+        moveSpeed = 30f;
+        PlayMode.mode = "Normal";
+        spitfire.transform.position = new Vector3(22f, 456, -904);
+        spitfire.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        spitfire.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        spitfire.transform.rotation = Quaternion.identity;
+        tutorialPanel.SetActive(false);
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        // Debug.Log(collision.collider.tag);
+        if (collision.collider.tag == "Target" || collision.collider.tag == "Terrain")
+        {
+            MusicSource.clip = MusicClip_GameOver;
+            MusicSource.Play(0);
+            isAlive = false;
+            spitfire.transform.position = new Vector3(22f, 456, -904);
+            spitfire.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            spitfire.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            spitfire.transform.rotation = Quaternion.identity;
+            gameOverPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Target" || other.gameObject.tag == "Terrain")
+        {
+            isAlive = false;
+            gameOverPanel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/TargetScript.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetScript : MonoBehaviour {
+
+    private bool isShooted_1;
+    private bool isShooted_2;
+    private bool isShooted_3;
+    private bool isShooted_4;
+    private bool isShooted_5;
+    private float timer_1 = 0.0f;
+    private float timer_2 = 0.0f;
+    private float timer_3 = 0.0f;
+    private float timer_4 = 0.0f;
+    private float timer_5 = 0.0f;
+    public GameObject target_1;
+    public GameObject target_2;
+    public GameObject target_3;
+    public GameObject target_4;
+    public GameObject target_5;
+    public Text scoreBoard;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isShooted_1)
+        {
+            timer_1 += Time.deltaTime;
+            if(timer_1 > 5.0f)
+            {
+                target_1.SetActive(true);
+                target_1.transform.position = new Vector3(-731f, 660f, 221f);
+                target_1.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                timer_1 = 0.0f;
+                isShooted_1 = false;
+            }
+        }
+        if (isShooted_2)
+        {
+            timer_2 += Time.deltaTime;
+            if (timer_2 > 5.0f)
+            {
+                target_2.SetActive(true);
+                target_2.transform.position = new Vector3(-81f, 460f, 368f);
+                target_2.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                timer_2 = 0.0f;
+                isShooted_2 = false;
+            }
+        }
+        if (isShooted_3)
+        {
+            timer_3 += Time.deltaTime;
+            if (timer_3 > 5.0f)
+            {
+                target_3.SetActive(true);
+                target_3.transform.position = new Vector3(368f, 760f, 418f);
+                target_3.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                timer_3 = 0.0f;
+                isShooted_3 = false;
+            }
+        }
+        if (isShooted_4)
+        {
+            timer_4 += Time.deltaTime;
+            if (timer_4 > 5.0f)
+            {
+                target_4.SetActive(true);
+                target_4.transform.position = new Vector3(-231f, 1060f, 428f);
+                target_4.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                timer_4 = 0.0f;
+                isShooted_4 = false;
+            }
+        }
+        if (isShooted_5)
+        {
+            timer_5 += Time.deltaTime;
+            if (timer_5 > 5.0f)
+            {
+                target_5.SetActive(true);
+                target_5.transform.position = new Vector3(-131f, 660f, 288f);
+                target_5.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                timer_5 = 0.0f;
+                isShooted_5 = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.collider.name == "Target_1")
+        {
+            target_1.SetActive(false);
+            isShooted_1 = true;
+            UpdateScore();
+        }
+        if (collision.collider.name == "Target_2")
+        {
+            target_2.SetActive(false);
+            isShooted_2 = true;
+            UpdateScore();
+        }
+        if (collision.collider.name == "Target_3")
+        {
+            target_3.SetActive(false);
+            isShooted_3 = true;
+            UpdateScore();
+        }
+        if (collision.collider.name == "Target_4")
+        {
+            target_4.SetActive(false);
+            isShooted_4 = true;
+            UpdateScore();
+        }
+        if (collision.collider.name == "Target_5")
+        {
+            target_5.SetActive(false);
+            isShooted_5 = true;
+            UpdateScore();
+        }
+    }
+
+    private void UpdateScore()
+    {
+        int currentScore = int.Parse(scoreBoard.text.Substring(7));
+        scoreBoard.text = "Score: " + (currentScore + 1).ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Target_1")
+        {
+            target_1.SetActive(false);
+            isShooted_1 = true;
+            UpdateScore();
+        }
+        if (other.gameObject.name == "Target_2")
+        {
+            target_2.SetActive(false);
+            isShooted_2 = true;
+            UpdateScore();
+        }
+        if (other.gameObject.name == "Target_3")
+        {
+            target_3.SetActive(false);
+            isShooted_3 = true;
+            UpdateScore();
+        }
+        if (other.gameObject.name == "Target_4")
+        {
+            target_4.SetActive(false);
+            isShooted_4 = true;
+            UpdateScore();
+        }
+        if (other.gameObject.name == "Target_5")
+        {
+            target_5.SetActive(false);
+            isShooted_5 = true;
+            UpdateScore();
+        }
+    }
+}
