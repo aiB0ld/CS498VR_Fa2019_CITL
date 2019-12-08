5ad877fcaee691763a2236b3e380e2877e13601a
aiB0ld <48540959+aiB0ld@users.noreply.github.com>
2019-12-08 17:34:15 -0600

    ui feedback

---
+++ Assets/Scripts/buttons/ButtonScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    //public GameObject target_1;
+    public GameObject learnobj;
+    public GameObject realPause;
+    public GameObject todolist;
+    public GameObject mapmap;
+    public Color onTouchColor;
+    private Button learnobjBtn;
+    public float grabBegin = 0.05f;
+    public float grabEnd = 0.05f;
+    private float m_prevFlex;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private bool CheckForGrabOrRelease(float prevFlex)
+    {
+        if ((m_prevFlex >= grabBegin) && (prevFlex < grabBegin))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            learnobjBtn = gameObject.GetComponent<Button>();
+            var colors = learnobjBtn.colors;
+            colors.normalColor = onTouchColor;
+            learnobjBtn.colors = colors;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        float prevFlex = m_prevFlex;
+        m_prevFlex = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            if (CheckForGrabOrRelease(prevFlex))
+            {
+                learnobjBtn = gameObject.GetComponent<Button>();
+                var colors = learnobjBtn.colors;
+                colors.normalColor = Color.white;
+                learnobjBtn.colors = colors;
+
+                if (gameObject.name == "learningobjectives")
+                {
+                    if (learnobj.activeInHierarchy)
+                    {
+                        learnobj.SetActive(false);
+                    }
+                    else
+                    {
+                        mapmap.SetActive(false);
+                        todolist.SetActive(false);
+                        learnobj.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                }
+                else if (gameObject.name == "map")
+                {
+                    if (mapmap.activeInHierarchy)
+                    {
+                        mapmap.SetActive(false);
+                    }
+                    else
+                    {
+                        todolist.SetActive(false);
+                        learnobj.SetActive(false);
+                        mapmap.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                }
+                else if (gameObject.name == "starmenu")
+                {
+                    SceneManager.LoadScene("StartMenu");
+                }
+                else if (gameObject.name == "todolist")
+                {
+                    if (todolist.activeInHierarchy)
+                    {
+                        todolist.SetActive(false);
+                    }
+                    else
+                    {
+                        mapmap.SetActive(false);
+                        learnobj.SetActive(false);
+                        todolist.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            learnobjBtn = gameObject.GetComponent<Button>();
+            var colors = learnobjBtn.colors;
+            colors.normalColor = Color.white;
+            learnobjBtn.colors = colors;
+        }
+    }
+}
--- Assets/Scripts/buttons/Objective.cs
+++ Assets/Scripts/buttons/Objective.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Objective : MonoBehaviour
 {
-
-
     //public GameObject target_1;
     public GameObject learnobj;
     public GameObject realPause;
     public GameObject todolist;
     public GameObject mapmap;
+    public Color onTouchColor;
+    private Button learnobjBtn;
+    public float grabBegin = 0.05f;
+    public float grabEnd = 0.05f;
+    private float m_prevFlex;
+
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -25,20 +29,96 @@ public class Objective : MonoBehaviour
 
     }
 
+    private bool CheckForGrabOrRelease(float prevFlex)
+    {
+        if ((m_prevFlex >= grabBegin) && (prevFlex < grabBegin))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Hand"))
         {
-            if (learnobj.activeInHierarchy)
+            learnobjBtn = gameObject.GetComponent<Button>();
+            var colors = learnobjBtn.colors;
+            colors.normalColor = onTouchColor;
+            learnobjBtn.colors = colors;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        float prevFlex = m_prevFlex;
+        m_prevFlex = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            if (CheckForGrabOrRelease(prevFlex))
             {
-                learnobj.SetActive(false);
-            } else
-            {
-                mapmap.SetActive(false);
-                todolist.SetActive(false);
-                learnobj.SetActive(true);
-                realPause.SetActive(false);
+                learnobjBtn = gameObject.GetComponent<Button>();
+                var colors = learnobjBtn.colors;
+                colors.normalColor = Color.white;
+                learnobjBtn.colors = colors;
+
+                if (gameObject.name == "learningobjectives")
+                {
+                    if (learnobj.activeInHierarchy)
+                    {
+                        learnobj.SetActive(false);
+                    }
+                    else
+                    {
+                        mapmap.SetActive(false);
+                        todolist.SetActive(false);
+                        learnobj.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                } else if (gameObject.name == "map") {
+                    if (mapmap.activeInHierarchy)
+                    {
+                        mapmap.SetActive(false);
+                    }
+                    else
+                    {
+                        todolist.SetActive(false);
+                        learnobj.SetActive(false);
+                        mapmap.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                } else if (gameObject.name == "starmenu")
+                {
+                    SceneManager.LoadScene("StartMenu");
+                } else if (gameObject.name == "todolist")
+                {
+                    if (todolist.activeInHierarchy)
+                    {
+                        todolist.SetActive(false);
+                    }
+                    else
+                    {
+                        mapmap.SetActive(false);
+                        learnobj.SetActive(false);
+                        todolist.SetActive(true);
+                        realPause.SetActive(false);
+                    }
+                }
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            learnobjBtn = gameObject.GetComponent<Button>();
+            var colors = learnobjBtn.colors;
+            colors.normalColor = Color.white;
+            learnobjBtn.colors = colors;
         }
     }
 }
