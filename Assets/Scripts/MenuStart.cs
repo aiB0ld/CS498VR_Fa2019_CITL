6f4fea3e2d8077df8032fbf52cffe8e577e431f7
aiB0ld <48540959+aiB0ld@users.noreply.github.com>
2019-12-08 13:39:34 -0600

    update

---
+++ Assets/Scripts/MenuStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.RawButton.Start))
+        {
+            Debug.Log("todolist");
+            SceneManager.LoadScene("ToDoList");
+        }
+        else if (OVRInput.GetDown(OVRInput.RawButton.A))
+        {
+          #if UNITY_EDITOR
+                      UnityEditor.EditorApplication.isPlaying = false;
+          #else
+                            Application.Quit();
+          #endif
+        }
+        else if (OVRInput.GetDown(OVRInput.RawButton.X))
+        {
+            //PlayMode.mode = "Tutorial";
+            //SceneManager.LoadScene("CS498HW4");
+        }
+    }
+}
